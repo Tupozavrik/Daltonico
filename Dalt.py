84de3107ed11bd07ecb2770adbdeb61ca521c4cf
Tupozavrik <128019578+Tupozavrik@users.noreply.github.com>
2024-05-05 03:29:39 +0300

    Add files via upload

---
+++ Dalt.py
@@ -0,0 +1,42 @@
+from tkinter import*
+import cv2
+import numpy as np
+def Cam ():
+    def change_saturation(frame, saturation_factor):
+        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        hsv_frame[:, :, 1] = np.clip(hsv_frame[:, :, 1] * saturation_factor, 0, 255).astype(np.uint8)
+        modified_frame = cv2.cvtColor(hsv_frame, cv2.COLOR_HSV2BGR)
+        return modified_frame
+
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()
+
+        if ret:
+            saturated_frame = change_saturation(frame, 3.5)
+            cv2.imshow('DALTONICO', saturated_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+root = Tk()
+
+root.title('DALTONICO')
+root.geometry('331x741')
+root.resizable(width = 'False', height = 'False')
+
+root.image = PhotoImage(file="fon.png")
+bg_image = Label(root, image=root.image)
+bg_image.pack()
+
+
+root.buttonImage = PhotoImage(file="knopka.png")
+roundedbutton = Button(root, bg = 'white', image=root.buttonImage, command= Cam)
+roundedbutton.place(x = 10 , y = 575)
+
+root.mainloop()
